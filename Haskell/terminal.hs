57d9d759444202ec03e226bdc196704ea06e5627
joaocfb <joao.bernardo@ccc.ufcg.edu.br>
2018-10-24 15:23:03 -0300

    Enviando o terminal implementado

--- Haskell/terminal.hs
+++ Haskell/terminal.hs
@@ -25,12 +25,7 @@ escolhaTerminal = putStrLn "Em qual terminal você se encontra?\n1. MONTEIRO\n2.
 -- TERMINAL
 
 cidades = [("MT","MONTEIRO"),("CG","CAMPINA GRANDE"),("JP","JOAO PESSOA"),("SS","SOUSA")]
-
-
---split :: String -> Char -> [String]
---split str delim = let (start, end) = break (== delim) str
---                    in start : if null end then [] else split (tail end) delim
-
+taxa = 1.5
 
 split str = case break (=='-') str of
                 (a, _comma:b) -> a : split b
@@ -43,28 +38,28 @@ toString array = mapM_ (putStrLn.show) array
 
 selec_prim [x, _] = x
 selec_sec [_, xs] = xs
---reverseList [] = []
---reverseList (x:xs) = reverseList xs ++ [x]
 
--- getCidades
-getCidadeByCodigo :: (String, String) -> [String] -> String
+reverseList [] = []
+reverseList (x:xs) = reverseList xs ++ [x]
+
+
+getCidadeByCodigo :: (String, String) -> [String] -> String -- metodo auxiliar
 getCidadeByCodigo (cod,cidade) sigla
       | (cod == selec_prim sigla) = cidade
       | (cod == selec_sec sigla) = cidade
       | otherwise = ""
 
-lista [] sigla = []
+lista [] sigla = [] -- metodo auxiliar
 lista (x:xs) sigla
   | (getCidadeByCodigo x sigla) == "" = lista xs sigla
   | otherwise = getCidadeByCodigo x sigla : lista xs sigla
 
-concatenate:: [String] -> String
+concatenate:: [String] -> String -- metodo auxiliar
 concatenate cidades = intercalate " - " cidades
 
 getCidadeFinal:: String -> String
 getCidadeFinal codigoByUsuario =
   concatenate (lista cidades (split codigoByUsuario))
-
 
 empresas = ["Real Bus", "Viacao Sao Jose", "Pontual Transporte", "Travel Bus"]
 rotas = [("MT-CG","CG-MT"),("CG-JP","JP-CG"),("MT-JP","JP-MT"),("MT-SS","SS-MT"),("SS-CG","CG-SS"),("SS-JP","JP-SS")]
@@ -72,13 +67,39 @@ rotas = [("MT-CG","CG-MT"),("CG-JP","JP-CG"),("MT-JP","JP-MT"),("MT-SS","SS-MT")
 busca_prim (x, _) = x
 busca_sec (_, xs) = xs
 
-getValoresByCodigo :: (String, String) -> String -> Int
+getValoresByCodigo :: (String, String) -> String -> Int -- metodo auxiliar
 getValoresByCodigo (sigla1,sigla2) siglaRota
       | (sigla1 == siglaRota) = 1
       | (sigla2 == siglaRota) = 2
       | otherwise = 0
 
---getRota [] sigla _ = 0
---getRota (x:xs) sigla cont
---  | ((getValoresByCodigo x sigla) == 0) = cont + (getRota xs sigla cont++)
---  | otherwise = (getValoresByCodigo x sigla) + getRota xs sigla cont++
+searchRota [] sigla = 0
+searchRota (x:xs) sigla -- metodo auxiliar
+      | (getValoresByCodigo x sigla) == 0 = 1 + (searchRota xs sigla)
+      | otherwise = 1 + (getValoresByCodigo x sigla)
+
+retornaPrecos:: String -> Float -- metodo auxiliar
+retornaPrecos sigla
+      | (searchRota rotas sigla) <= 3 = 26.0
+      | (searchRota rotas sigla) <= 5 = 28.0
+      | (searchRota rotas sigla) <= 6 = 32.0
+      | otherwise = 35.0
+
+destinosEValores :: (String,String) -> [String]
+destinosEValores (sigla1, sigla2) =
+      [getCidadeFinal sigla1 ++ " - R$ " ++ show (retornaPrecos sigla1)] ++ [getCidadeFinal sigla2 ++ " - R$ " ++ show (retornaPrecos sigla2)]
+
+
+listaRotas [] = []
+listaRotas (x:xs) = destinosEValores x : listaRotas xs
+
+getTaxaDeEmbarque :: Int -> Float
+getTaxaDeEmbarque qtdMalas = fromIntegral (qtdMalas) * taxa
+
+getNomeCidade [] sigla = []
+getNomeCidade (x:xs) sigla
+      | sigla == (busca_prim x) = busca_sec x
+      | otherwise = getNomeCidade xs sigla
+
+getInfo :: String -> String
+getInfo codigo = "Terminal Rodoviario de " ++ (getNomeCidade cidades codigo) ++ " Empresas disponiveis: Real Bus, Viacao Sao Jose, Pontual Transporte, Travel Bus"
